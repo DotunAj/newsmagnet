8f9ff8f765d248051d9c240758b6ff897e3324d4
Ajala Ayodotun <dotunajala@gmail.com>
2018-06-21 07:38:14 +0100

    Offline first initiated

--- build/sw.js
+++ build/sw.js
@@ -1,7 +1,15 @@
 'use strict';
 
-self.addEventListener('fetch', function (event) {
-    console.log(event.request);
+self.addEventListener('install', function (event) {
+    event.waitUntil(caches.open('newsmagnet-v1').then(function (cache) {
+        return cache.addAll(['/', 'css/main.css', 'js/main.js', 'https://use.fontawesome.com/releases/v5.0.10/js/all.js', 'assets/fallback.jpeg']);
+    }));
 });
 
-//boo
+self.addEventListener('fetch', function (event) {
+    //Replaces broken images with fallback image
+    event.respondWith(caches.match(event.request).then(function (response) {
+        //if(response.status >= 300  && (response.url.match(/\.(jpeg|jpg|git|png)$/) != null) ) return caches.match('assets/fallback.jpeg');
+        return response || fetch(event.request);
+    }));
+});
--- src/js/sw.js
+++ src/js/sw.js
@@ -1,6 +1,27 @@
-self.addEventListener('fetch', (event) => {
-    console.log(event.request);
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('newsmagnet-v1')
+        .then((cache) => {
+            return cache.addAll([
+                '/',
+                'css/main.css',
+                'js/main.js',
+                'https://use.fontawesome.com/releases/v5.0.10/js/all.js',
+                'assets/fallback.jpeg'
+            ]);
+        })
+    )
 })
 
-//boo
+self.addEventListener('fetch', (event) => {
+    //Replaces broken images with fallback image
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                //if(response.status >= 300  && (response.url.match(/\.(jpeg|jpg|git|png)$/) != null) ) return caches.match('assets/fallback.jpeg');
+                return response || fetch(event.request);
+            })
+    )
+})
+
 
